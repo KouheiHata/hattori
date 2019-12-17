13da4b4122168af4876abec3df377f70f09b97d4
hirotaka <h_yamaguchi@standtogether-fukushi.com>
2019-12-17 10:46:25 +0900

    食用菊部分制作12/17

--- page-daisy.php
+++ page-daisy.php
@@ -10,24 +10,33 @@ Template Name: daisy
 <div class="container">
    <div class="row">
     <h4 class="section_h">食用菊って？</h4>
-    <div class="col-lg-4 col-md-4 col-sm-4">
+    <div class="col-lg-4 col-md-4 col-sm-12">
         <img class="desc-img" src="<?php echo esc_url( get_theme_file_uri('images/eat-daigy.jpg') ); ?>" alt="食用菊｜黄">
     </div>
-    <div class="col-lg-8 col-md-8 col-sm-8">
+    <div class="col-lg-8 col-md-8 col-sm-12">
         <p class="desc-p">食用菊はその名の通り「食用に栽培されている菊」のことで観賞用の菊と比べて苦みが少なく、甘みがあるのが特徴です。<br>
         刺身の盛り合わせに食用菊が添えられているのを見たことがある方は多いと思いますが、食用菊はそれだけでなく、おひたしや和え物・酢の物・天ぷらなど様々な料理のレパートリーがあります</p>
     </div>
-    </div>
     <h4 class="section_h">ハウスでの取り扱いの食用菊</h4>
-    <div class="col-lg-6">
+            <div class="col-lg-6">
         <img class="desc-img" src="<?php echo esc_url( get_theme_file_uri('images/enmeiraku.jpg') ); ?>" alt="延命楽（えんめいらく）">
-        <h5 class="card-title">延命楽（えんめいらく）</h5>
+        <h5 class="desc-title">延命楽（えんめいらく）</h5>
         <p class="desc-p">紫色の花びらが特徴の食用菊で、食用菊の中で最も味・香り・食感が良いとされています。</p>
     </div>
-    <div class="col-lg-6"></div>
+    <div class="col-lg-6">
+        <img src="<?php echo esc_url( get_theme_file_uri('images/aboukyu.jpg') ); ?>" alt="阿房宮（あぼうきゅう）" class="desc-img">
+        <h5 class="desc-title">阿房宮（あぼうきゅう）</h5>
+        <p class="desc-p">明るい黄色の食用菊で、延命楽より小ぶりの食用菊です。<br>食感は延命楽より柔らかめです。</p>
+    </div>
+    
     <section class="section">
     <p class="main_p">ご注文は下記注文票をダウンロードしてご希望の品目を記入し、<br>ＦＡＸか当ハウスまで直接お持ちください。</p>
     <a class="d_g_pdf" href="#">2019年度苗注文票のダウンロードはこちら<i class="far fa-file-pdf"></i></a>
 </section>
+<h4 class="section_h">おいしいレシピ</h4>
+<div class="col-lg-4 col-md-4 col-sm-12">
+    
+</div>
+    </div>
 </div>
 <?php get_footer(); ?>

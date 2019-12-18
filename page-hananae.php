5fbd5b9d8e8ef6208df309a5bd74d531bb4c4b10
hirotaka <h_yamaguchi@standtogether-fukushi.com>
2019-12-18 11:09:41 +0900

    花苗部分制作12/18

--- page-hananae.php
+++ page-hananae.php
@@ -13,34 +13,175 @@ Template Name: hananae
 <div class="container">
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12">
-          
+         <div class="container">
+             <div class="row">
+                 <div class="col-lg-4 col-md-4 col-sm-12 px-5">
+                     <h2 class="flower_h">４月</h2>
+                     <img class="f-img" src="<?php echo esc_url( get_theme_file_uri('images/petunia.jpg') ); ?>" alt="">
+                     <table class="table f-table">
+                         <thead class="f-name">
+                         <tr><th>ペチュニア</th></tr>
+                         </thead>
+                         <tbody>
+                             <tr>
+                                 <td>色：赤　紫　白</td>
+                             </tr>
+                             <tr>
+                                 <td>－－円</td>
+                             </tr>
+                         </tbody>
+                     </table>
+                 </div>
+                 <div class="col-lg-4 col-md-4 col-sm-12 px-5">
+                     <h2 class="flower_h">10月</h2>
+                     <img src="<?php echo esc_url( get_theme_file_uri('images/begonia.jpg')); ?>" alt="" class="f-img">
+                     <table class="table f-table">
+                         <thead class="f-name">
+                             <tr>
+                                 <th>ベゴニア</th>
+                             </tr>
+                         </thead>
+                         <tbody>
+                             <tr>
+                                 <td>色：－－</td>
+                             </tr>
+                             <tr>
+                                 <td>ーー円</td>
+                             </tr>
+                         </tbody>
+                     </table>
+                 </div>
+                 <div class="col-lg-4 col-md-4 col-sm-12 px-5">
+                     <h2 class="flower_h">11月</h2>
+                     <img src="<?php echo esc_url( get_theme_file_uri('images/pangy.jpg') ); ?>" alt="" class="f-img">
+                     <table class="table f-table">
+                         <thead class="f-name">
+                             <tr>
+                                 <th>パンジー</th>
+                             </tr>
+                         </thead>
+                         <tbody>
+                             <tr>
+                                 <td>色：－－</td>
+                             </tr>
+                             <tr>
+                                 <td>－－円</td>
+                             </tr>
+                         </tbody>
+                     </table>
+                 </div>
+             </div>
+         </div>
            </div>
-            <div class="col-lg-4 col-md-4 col-sm-4">
-               <h2 class="card_h">4月</h2>
-           <div class="card">
-               <div class="card-body">
-                   <img class="card-img" src="<?php echo esc_url( get_theme_file_uri('images/petunia.jpg') ); ?>" alt="">
-               </div>
-            </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-4">
-                <h2 class="card_h">10月</h2>
-                <div class="card">
-                    <div class="card-body"></div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-4">
-                <h2 class="card_h">11月</h2>
-                <div class="card">
-                    <div class="card-body"></div>
-                </div>
-            </div>
     </div>
 </div>
 
 <h4 class="section_h">切花苗</h4>
+<div class="col-lg-12 col-md-12 col-sm-12">
+   <h2 class="flower_h w-75">７月</h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12 px-5">
+    <img src="<?php echo esc_url( get_theme_file_uri('images/zinnia.jpg') ); ?>" alt="百日草" class="f-img">
+    <table class="table f-table">
+        <thead class="f-name">
+            <tr>
+                <th>百日草</th>
+                <tbody>
+                    <tr>
+                        <td>色：赤　白　黄　桃</td>
+                    </tr>
+                    <tr><td>－－円</td></tr>
+                </tbody>
+            </tr>
+        </thead>
+    </table>
+</div>
+<div class="col-lg-4 col-md-4 col-sm-12 px-5">
+    <img src="<?php echo esc_url( get_theme_file_uri('images/amaranth.jpg') ); ?>" alt="千日草" class="f-img">
+    <table class="table f-table">
+        <thead class="f-name">
+            <tr>
+                <th>千日草</th>
+                <tbody>
+                    <tr>
+                        <td>色：赤　白　黄　紫</td>
+                    </tr>
+                    <tr>
+                        <td>－－円</td>
+                    </tr>
+                </tbody>
+            </tr>
+        </thead>
+    </table>
+</div>
+<div class="col-lg-4 col-md-4 col-sm-12 px-5">
+    <img src="<?php echo esc_url( get_theme_file_uri('images/aster01_02.jpg') ); ?>" alt="アスター" class="f-img">
+    <table class="table f-table">
+        <thead class="f-name">
+            <tr>
+                <th>アスター</th>
+                <tbody>
+                    <tr>
+                        <td>色：－－</td>
+                    </tr>
+                    <tr>
+                        <td>－－円</td>
+                    </tr>
+                </tbody>
+            </tr>
+        </thead>
+    </table>
+</div>
+        </div>
+    </div>
+</div>
+
 
 <h4 class="section_h">菊苗</h4>
+       <h2 class="flower_h w-75">１０月</h2>
+        <div class="col-lg-12 col-md-12 col-sm-12">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-12 px-5">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/potmom.jpg') ); ?>" alt="ポットマム" class="f-img">
+                        <table class="table f-table">
+                            <thead class="f-name">
+                                <tr>
+                                    <th>ポットマム</th>
+                                    <tbody>
+                                        <tr>
+                                            <td>色：－－</td>
+                                        </tr>
+                                        <tr>
+                                            <td>－－円</td>
+                                        </tr>
+                                    </tbody>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-12 px-5">
+                        <img src="<?php echo esc_url( get_theme_file_uri('images/cushionmom.jpg') ); ?>" alt="クッションマム" class="f-img">
+                        <table class="table f-table">
+                            <thead class="f-name">
+                                <tr>
+                                    <th>クッションマム</th>
+                                    <tbody>
+                                        <tr>
+                                            <td>色：－－</td>
+                                        </tr>
+                                        <tr>
+                                            <td>－－円</td>
+                                        </tr>
+                                    </tbody>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 <section class="section">
     <p class="main_p">ご注文は下記注文票をダウンロードしてご希望の品目を記入し、<br>ＦＡＸか当ハウスまで直接お持ちください。</p>

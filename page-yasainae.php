c21bfd6748bcc3974de7bd3840099151e17352b4
hirotaka <h_yamaguchi@standtogether-fukushi.com>
2019-12-19 13:55:21 +0900

    野菜苗12/19

--- page-yasainae.php
+++ page-yasainae.php
@@ -13,19 +13,51 @@ Template Name: yasainae
         ご注文はページ下記の注文票をダウンロードください
     </p>
     <div class="dgloup">
-    <p class="list_title">春野菜</p>
-    <select name="spring" class="d-list" id="">
-        <option value="なす">なす</option>
-    </select>
-    <p class="list_title">秋野菜</p>
-    <select name="autumn" class="d-list" id="">
-        <option value="選択してください" selected>選択してください</option>
-    </select>
+        <p class="list_title">春野菜</p>
+        <select name="spring" class="d-list" id="">
+            <option value="選択してください" selected>選択してください</option>
+            <option value="なす">なす</option>
+        </select>
+        <p class="list_title">秋野菜</p>
+        <select name="autumn" class="d-list" id="">
+            <option value="選択してください" selected>選択してください</option>
+        </select>
     </div>
 </section>
-
-<h4 class="section_h">春野菜苗</h4>
-<h2 class="category_h">なす（ナス科）</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 col-md12 col-sm-12">
+            <h4 class="section_h">春野菜苗</h4>
+            <h2 class="category_h">なす（ナス科）</h2>
+        </div>
+        <div class="col-lg-3 col-md-3 col-sm-12 px-5">
+            <img src="<?php echo esc_url( get_theme_file_uri('images/nath_03.jpg') ); ?>" alt="">
+        </div>
+        <div class="col-lg-9 col-md-9 col-sm-12 px-5">
+            <table class="table b-table">
+               <thead><tr>
+                   <th class="b-cell">品名</th>
+                   <th class="b-cell">自根</th>
+                   <th class="b-cell">接木</th>
+               </tr></thead>
+                <tbody>
+                    <tr>
+                        <td class="b-cell">千両２号長茄子</td>
+                        <td class="b-cell text-right">　　　　円</td>
+                        <td class="b-cell text-right">　円</td>
+                    </tr>
+                    <tr>
+                        <td class="b-cell">早生大丸茄子</td>
+                        <td class="b-cell text-right">　円</td>
+                        <td class="b-cell text-right">　円</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <h4 class="section_h">秋野菜苗</h4>
+        <h2 class="category_h">なす（ナス科）</h2>
+    </div>
+</div>
 
 <section class="section">
     <p class="main_p">ご注文は下記注文票をダウンロードしてご希望の品目を記入し、<br>ＦＡＸか当ハウスまで直接お持ちください。</p>

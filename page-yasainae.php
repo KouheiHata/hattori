0bbe73f531b654551062acdcf7377a21923fc523
Kouhei Hata <k_hata@standtogether-fukushi.com>
2019-12-20 15:29:18 +0900

    野菜苗
    表示、非表示の制御(JavaScript)

--- page-yasainae.php
+++ page-yasainae.php
@@ -6,6 +6,35 @@ Template Name: yasainae
 
 
 <?php get_header(); ?>
+
+<script type="text/javascript">
+function entryChange(){
+	if(document.getElementById('st_haru')){
+		id = document.getElementById('st_haru').value;
+
+		if(id != '0'){
+			document.getElementById('haruyasai').style.display = "";
+			document.getElementById(id).style.display = "";
+		}else if(id == '0'){
+			document.getElementById('haruyasai').style.display = "none";
+			document.getElementById('nasu').style.display = "none";
+		}
+	}
+	if(document.getElementById('st_aki')){
+		id = document.getElementById('st_aki').value;
+
+		if(id != '0'){
+			document.getElementById('akiyasai').style.display = "";
+			document.getElementById(id).style.display = "";
+		}else if(id == '0'){
+			document.getElementById('akiyasai').style.display = "none";
+			document.getElementById('daikon').style.display = "none";
+		}
+	}
+}
+window.onload = entryChange;
+</script>
+
 <img src="<?php echo esc_url( get_theme_file_uri('images/main-img3.jpg') ); ?>" alt="">
 <section class="section">
     <p class="main_p">
@@ -14,26 +43,69 @@ Template Name: yasainae
     </p>
     <div class="dgloup">
         <p class="list_title">春野菜</p>
-        <select name="spring" class="d-list" id="">
-            <option value="選択してください" selected>選択してください</option>
-            <option value="なす">なす</option>
+        <select name="spring" class="d-list" id="st_haru" onchange="entryChange();">
+            <option value="0" selected>選択してください</option>
+            <option value="nasu">なす</option>
         </select>
         <p class="list_title">秋野菜</p>
-        <select name="autumn" class="d-list" id="">
-            <option value="選択してください" selected>選択してください</option>
+        <select name="autumn" class="d-list" id="st_aki" onchange="entryChange();">
+            <option value="0" selected>選択してください</option>
+            <option value="daikon">大根</option>
         </select>
     </div>
 </section>
 <div class="container">
     <div class="row">
         <div class="col-lg-12 col-md12 col-sm-12">
-            <h4 class="section_h">春野菜苗</h4>
-            <h2 class="category_h">なす（ナス科）</h2>
+            <h4 class="section_h" id="haruyasai">春野菜苗</h4>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-12 px-5">
+	</div>
+		<div id="nasu">
+		    <div class="row">
+	        <div class="col-lg-12 col-md12 col-sm-12">
+	            <h2 class="category_h">なす（ナス科）</h2>
+	        </div>
+	        <div class="col-lg-3 col-md-3 col-sm-12">
+	        <img src="<?php echo esc_url( get_theme_file_uri('images/nath_03.jpg') ); ?>" alt="">
+			</div>
+	        <div class="col-lg-9 col-md-9 col-sm-12 px-5">
+	            <table class="table b-table">
+	               <thead><tr>
+	                   <th class="b-cell">品名</th>
+	                   <th class="b-cell">自根</th>
+	                   <th class="b-cell">接木</th>
+	               </tr></thead>
+	                <tbody>
+	                    <tr>
+	                        <td class="b-cell">千両２号長茄子</td>
+	                        <td class="b-cell text-right">　　　　円</td>
+	                        <td class="b-cell text-right">　円</td>
+	                    </tr>
+	                    <tr>
+	                        <td class="b-cell">早生大丸茄子</td>
+	                        <td class="b-cell text-right">　円</td>
+	                        <td class="b-cell text-right">　円</td>
+	                    </tr>
+	                </tbody>
+	            </table>
+	        </div>
+			</div>	<!-- row -->
+		</div>		<!-- nasu -->
+
+    <div class="row">
+        <div class="col-lg-12 col-md12 col-sm-12">
+	        <h4 class="section_h" id="akiyasai">秋野菜苗</h4>
+        </div>
+	</div>
+		<div id="daikon">
+		    <div class="row">
+	        <div class="col-lg-12 col-md12 col-sm-12">
+	        <h2 class="category_h">大根（アブラナ科）</h2>
+	        </div>
+	        <div class="col-lg-3 col-md-3 col-sm-12">
             <img src="<?php echo esc_url( get_theme_file_uri('images/nath_03.jpg') ); ?>" alt="">
-        </div>
-        <div class="col-lg-9 col-md-9 col-sm-12 px-5">
+			</div>
+	        <div class="col-lg-9 col-md-9 col-sm-12 px-5">
             <table class="table b-table">
                <thead><tr>
                    <th class="b-cell">品名</th>
@@ -42,22 +114,20 @@ Template Name: yasainae
                </tr></thead>
                 <tbody>
                     <tr>
-                        <td class="b-cell">千両２号長茄子</td>
+                        <td class="b-cell">雪美人</td>
                         <td class="b-cell text-right">　　　　円</td>
                         <td class="b-cell text-right">　円</td>
                     </tr>
                     <tr>
-                        <td class="b-cell">早生大丸茄子</td>
+                        <td class="b-cell">ころっ娘</td>
                         <td class="b-cell text-right">　円</td>
                         <td class="b-cell text-right">　円</td>
                     </tr>
                 </tbody>
             </table>
-        </div>
-        <h4 class="section_h">秋野菜苗</h4>
-        <h2 class="category_h">なす（ナス科）</h2>
-    </div>
-</div>
+	        </div>
+			</div>	<!-- row -->
+		</div>		<!-- daikon -->
 
 <section class="section">
     <p class="main_p">ご注文は下記注文票をダウンロードしてご希望の品目を記入し、<br>ＦＡＸか当ハウスまで直接お持ちください。</p>

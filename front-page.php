00296d22cf457a690944b3933eb2e0f988bfe0a0
Kouhei Hata <k_hata@standtogether-fukushi.com>
2020-01-07 14:24:05 +0900

    スケジュール表のtableを廃止した
    divタグで枠線を構築

--- front-page.php
+++ front-page.php
@@ -9,77 +9,107 @@ Template Name: フロントページ
 
 <img class="main-img" src="<?php echo esc_url( get_theme_file_uri('images/main-img.jpg') ); ?>" alt="苗のはっとりは福井県越前市にある、ハウス直販の野菜・花苗の専門店です。育てやすく元気な苗を販売しております。また、苗の販売だけでなく、育て方のアドバイスなども行っています。">
 
-<div class="container-fluid top_back">
+<div class="container-fluid">
 <div class="row">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
 
-		<img src="<?php echo get_theme_file_uri('images/schedule_top.png'); ?>" alt="年間スケジュール" class="pt-5 text-center" width="80%"><br />
+		<img src="<?php echo get_theme_file_uri('images/schedule_top.png'); ?>" alt="年間スケジュール" class="pt-5 text-center" width="960px"><br />
 
 	</div>
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
-		<div style="background-color:#ffffff; width:80%; margin: 0 auto;">
+		<div style="background-color:#ffffff; width:960px; height:760px; margin: 0 auto;">
 
-		<table border="0">
-		<tr><td width="250px">
-			<span class="sc_tuki">３</span><span class="h1 pr-3">月</span>
-		</td><td class="pl-4 pt-3 float-left lh15 sc_back2">
+		<div class="top_border_x float-left ml-5 pr-3" style="border-width: 0px 0px 3px 0px; width: 170px;">
+			<span class="sc_tuki">３</span><span class="h1">月</span>
+		</div>
+
+		<div class="top_border_y float-left m-0 p-0" style="border-width: 0px 0px 0px 3px;"></div>
+
+		<div class="top_border_x lh15 float-left pl-3 pt-3" style="border-width: 0px 0px 3px 0px; width: 700px;">
 			<span style="color: #ed1c24;">■</span>野菜作り交流会（要予約）<br />
 			3/15　10:00～
-		</td></tr>
-		<tr><td class="sc_back" style="background-position: 50px 0px;">
-			<span class="sc_tuki">４</span><span class="h1 pr-3">月</span>
-		</td><td class="pl-4 pt-3 float-left lh15 sc_back2">
-			<img src="<?php echo get_theme_file_uri('images/border_sch.png'); ?>" class="sc_border_x"><br />
+		</div>
+
+
+		<div class="top_border_x float-left ml-5 pr-3" style="border-width: 0px 0px 3px 0px; width: 170px;">
+			<span class="sc_tuki">４</span><span class="h1">月</span>
+		</div>
+
+		<div class="top_border_y float-left m-0 p-0" style="border-width: 0px 0px 0px 3px;"></div>
+
+		<div class="top_border_x lh15 float-left pl-3 pt-3" style="border-width: 0px 0px 3px 0px; width: 700px;">
 			<span style="color: #8cc63f;">■</span>春野菜苗の販売<br />
 			4/29～5/12<br />
 			<i class="far fa-file-pdf"></i><a href="#">注文票ダウンロード</a>
-		</td></tr>
-		<tr><td class="sc_back" style="background-position: 50px 0px;">
-			<span class="sc_tuki">５</span><span class="h1 pr-3">月</span>
-		</td><td class="pl-4 pt-3 float-left lh15 sc_back2">
-			<img src="<?php echo get_theme_file_uri('images/border_sch.png'); ?>" class="sc_border_x"><br />
-			<table border="0">
-			<tr><td class="px-1 lh15">
+		</div>
+
+
+		<div class="top_border_x float-left ml-5 pr-3" style="border-width: 0px 0px 3px 0px; width: 170px;">
+			<span class="sc_tuki">５</span><span class="h1">月</span>
+		</div>
+
+		<div class="top_border_y float-left m-0 p-0" style="border-width: 0px 0px 0px 3px;"></div>
+
+		<div class="top_border_x lh15 float-left pl-3 pt-3" style="border-width: 0px 0px 3px 0px; width: 700px;">
+			<div class="d-block float-left">
 				<span style="color: #8cc63f;">■</span>春野菜苗の販売<br />
 				　4/29～5/12<br />
 				<i class="far fa-file-pdf"></i><a href="#">注文票ダウンロード</a>
-			</td><td class="px-1 lh15">
+			</div>
+
+			<div class="d-block float-left pl-3">
 				<span style="color: #8cc63f;">■</span>オクラ・ゴーヤの販売<br />
 				　5/10～<br />
 				<i class="far fa-file-pdf"></i><a href="#">注文票ダウンロード</a>
-			</td><td class="px-1 lh15">
+			</div>
+
+			<div class="d-block float-left pl-3">
 				<span style="color: #ed1c24;">■</span>野菜作りの交流会（要予約）<br />
 				　5/15 10:00～<br />
 				<br />
-			</td></tr>
-			</table>
-		</td></tr>
-		<tr><td class="sc_back" style="background-position: 50px 0px;">
-			<span class="sc_tuki">９</span><span class="h1 pr-3">月</span>
-		</td><td class="pl-4 pt-3 float-left lh15 sc_back2">
-			<img src="<?php echo get_theme_file_uri('images/border_sch.png'); ?>" class="sc_border_x"><br />
+			</div>
+		</div>
+
+
+		<div class="top_border_x float-left ml-5 pr-3" style="border-width: 0px 0px 3px 0px; width: 170px;">
+			<span class="sc_tuki">９</span><span class="h1">月</span>
+		</div>
+
+		<div class="top_border_y float-left m-0 p-0" style="border-width: 0px 0px 0px 3px;"></div>
+
+		<div class="top_border_x lh15 float-left pl-3 pt-3" style="border-width: 0px 0px 3px 0px; width: 700px;">
 			<span style="color: #8cc63f;">■</span>秋野菜苗の販売<br />
 			9/1～9/15<br />
 			<i class="far fa-file-pdf"></i><a href="#">注文票ダウンロード</a>
-		</td></tr>
-		<tr><td class="sc_back" style="background-position: 50px 0px;">
-			<span class="sc_tuki">11</span><span class="h1 pr-3">月</span>
-		</td><td class="pl-4 pt-3 float-left lh15 sc_back2">
-			<img src="<?php echo get_theme_file_uri('images/border_sch.png'); ?>" class="sc_border_x"><br />
+		</div>
+
+
+		<div class="top_border_x float-left ml-5 pr-3" style="border-width: 0px 0px 3px 0px; width: 170px;">
+			<span class="sc_tuki">11</span><span class="h1">月</span>
+		</div>
+
+		<div class="top_border_y float-left m-0 p-0" style="border-width: 0px 0px 0px 3px;"></div>
+
+		<div class="top_border_x lh15 float-left pl-3 pt-3" style="border-width: 0px 0px 3px 0px; width: 700px;">
 			<span style="color: #8cc63f;">■</span>玉ねぎ苗の販売<br />
 			11/1～11/10<br />
 			<i class="far fa-file-pdf"></i><a href="#">注文票ダウンロード</a>
-		</td></tr>
-		<tr><td class="sc_back" style="background-position: 50px 0px;">
-			<span class="sc_tuki">12</span><span class="h1 pr-3">月</span>
-		</td><td class="pl-4 pt-3 float-left lh15 sc_back2">
-			<img src="<?php echo get_theme_file_uri('images/border_sch.png'); ?>" class="sc_border_x"><br />
+		</div>
+
+
+		<div class="top_border_x float-left ml-5 pr-3" style="border-width: 0px 0px 3px 0px; width: 170px;">
+			<span class="sc_tuki">12</span><span class="h1">月</span>
+		</div>
+
+		<div class="top_border_y float-left m-0 p-0" style="border-width: 0px 0px 0px 3px;"></div>
+
+		<div class="top_border_x lh15 float-left pl-3 pt-3" style="border-width: 0px 0px 3px 0px; width: 700px;">
 			<span style="color: #ed1c24;">■</span>寄せ植え体験（要予約）<br />
 			12/1～12/15<br />
-		</td></tr>
-		</table>
-	</div>
+		</div>
+
+	</div> <!-- #ffffff -->
 
 	</div> <!-- col-lg-12 -->
 </div> <!-- row -->

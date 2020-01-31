d010ae038805d0d40cedd1ad6bca0573294209bc
Kouhei Hata <k_hata@standtogether-fukushi.com>
2020-01-31 13:28:01 +0900

    トップ：年間スケジュールのレスポンシブ対応
    イベントが３つの月は個別にクラス宣言する必要があった。

--- front-page.php
+++ front-page.php
@@ -13,98 +13,97 @@ Template Name: フロントページ
 <div class="row">
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
 
-		<img src="<?php echo get_theme_file_uri('images/schedule_top.png'); ?>" alt="年間スケジュール" class="pt-5 text-center" width="960px"><br />
+		<img src="<?php echo get_theme_file_uri('images/schedule_top.png'); ?>" alt="年間スケジュール" class="pt-5 text-center" width="98%"><br />
 
 	</div>
 	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 
-		<div style="background-color:#ffffff; width:960px; height:760px; margin: 0 auto;">
+		<div class="sc_board">
 
-		<div class="top_border_x float-left ml-5 pr-3" style="border-width: 0px 0px 3px 0px; width: 170px;">
-			<span class="sc_tuki">３</span><span class="h1">月</span>
+		<div class="top_border_x sc_tuki_size float-left ml-sm-5 pr-3" style="border-width: 0px 0px 3px 0px;">
+			<span class="sc_tuki">３</span><span class="sc_tuki2">月</span>
 		</div>
 
 		<div class="top_border_y float-left m-0 p-0" style="border-width: 0px 0px 0px 3px;"></div>
 
-		<div class="top_border_x lh15 float-left pl-3 pt-3" style="border-width: 0px 0px 3px 0px; width: 700px;">
+		<div class="top_border_x sc_detail_size lh15" style="border-width: 0px 0px 3px 0px;">
 			<span style="color: #ed1c24;">■</span>野菜作り交流会（要予約）<br />
 			3/15　10:00～
 		</div>
 
 
-		<div class="top_border_x float-left ml-5 pr-3" style="border-width: 0px 0px 3px 0px; width: 170px;">
-			<span class="sc_tuki">４</span><span class="h1">月</span>
+		<div class="top_border_x sc_tuki_size float-left ml-sm-5 pr-3" style="border-width: 0px 0px 3px 0px;">
+			<span class="sc_tuki">４</span><span class="sc_tuki2">月</span>
 		</div>
 
 		<div class="top_border_y float-left m-0 p-0" style="border-width: 0px 0px 0px 3px;"></div>
 
-		<div class="top_border_x lh15 float-left pl-3 pt-3" style="border-width: 0px 0px 3px 0px; width: 700px;">
+		<div class="top_border_x sc_detail_size lh15" style="border-width: 0px 0px 3px 0px;">
 			<span style="color: #8cc63f;">■</span>春野菜苗の販売<br />
 			4/29～5/12<br />
 			<i class="far fa-file-pdf"></i><a href="#">注文票ダウンロード</a>
 		</div>
 
 
-		<div class="top_border_x float-left ml-5 pr-3" style="border-width: 0px 0px 3px 0px; width: 170px;">
-			<span class="sc_tuki">５</span><span class="h1">月</span>
+		<div class="top_border_x sc_tuki_size_3 float-left ml-sm-5 pr-3" style="border-width: 0px 0px 3px 0px;">
+			<span class="sc_tuki">５</span><span class="sc_tuki2">月</span>
 		</div>
 
-		<div class="top_border_y float-left m-0 p-0" style="border-width: 0px 0px 0px 3px;"></div>
+		<div class="top_border_y_3 float-left m-0 p-0" style="border-width: 0px 0px 0px 3px;"></div>
 
-		<div class="top_border_x lh15 float-left pl-3 pt-3" style="border-width: 0px 0px 3px 0px; width: 700px;">
-			<div class="d-block float-left">
+		<div class="top_border_x sc_detail_size_3 lh15" style="border-width: 0px 0px 3px 0px;">
+			<div class="d-block float-left pr-3">
 				<span style="color: #8cc63f;">■</span>春野菜苗の販売<br />
 				　4/29～5/12<br />
 				<i class="far fa-file-pdf"></i><a href="#">注文票ダウンロード</a>
 			</div>
 
-			<div class="d-block float-left pl-3">
+			<div class="d-block float-left pr-3">
 				<span style="color: #8cc63f;">■</span>オクラ・ゴーヤの販売<br />
 				　5/10～<br />
 				<i class="far fa-file-pdf"></i><a href="#">注文票ダウンロード</a>
 			</div>
 
-			<div class="d-block float-left pl-3">
+			<div class="d-block float-left">
 				<span style="color: #ed1c24;">■</span>野菜作りの交流会（要予約）<br />
 				　5/15 10:00～<br />
-				<br />
 			</div>
 		</div>
 
 
-		<div class="top_border_x float-left ml-5 pr-3" style="border-width: 0px 0px 3px 0px; width: 170px;">
-			<span class="sc_tuki">９</span><span class="h1">月</span>
+		<div class="top_border_x sc_tuki_size float-left ml-sm-5 pr-3" style="border-width: 0px 0px 3px 0px;">
+			<span class="sc_tuki">９</span><span class="sc_tuki2">月</span>
 		</div>
 
 		<div class="top_border_y float-left m-0 p-0" style="border-width: 0px 0px 0px 3px;"></div>
 
-		<div class="top_border_x lh15 float-left pl-3 pt-3" style="border-width: 0px 0px 3px 0px; width: 700px;">
+		<div class="top_border_x sc_detail_size lh15" style="border-width: 0px 0px 3px 0px;">
 			<span style="color: #8cc63f;">■</span>秋野菜苗の販売<br />
 			9/1～9/15<br />
 			<i class="far fa-file-pdf"></i><a href="#">注文票ダウンロード</a>
 		</div>
 
 
-		<div class="top_border_x float-left ml-5 pr-3" style="border-width: 0px 0px 3px 0px; width: 170px;">
-			<span class="sc_tuki">11</span><span class="h1">月</span>
+		<div class="top_border_x sc_tuki_size float-left ml-sm-5 pr-3" style="border-width: 0px 0px 3px 0px;">
+			<span class="sc_tuki">11</span><span class="sc_tuki2">月</span>
 		</div>
 
 		<div class="top_border_y float-left m-0 p-0" style="border-width: 0px 0px 0px 3px;"></div>
 
-		<div class="top_border_x lh15 float-left pl-3 pt-3" style="border-width: 0px 0px 3px 0px; width: 700px;">
+		<div class="top_border_x sc_detail_size lh15" style="border-width: 0px 0px 3px 0px;">
 			<span style="color: #8cc63f;">■</span>玉ねぎ苗の販売<br />
 			11/1～11/10<br />
 			<i class="far fa-file-pdf"></i><a href="#">注文票ダウンロード</a>
 		</div>
 
 
-		<div class="top_border_x float-left ml-5 pr-3" style="border-width: 0px 0px 3px 0px; width: 170px;">
-			<span class="sc_tuki">12</span><span class="h1">月</span>
+		<div class="top_border_x sc_tuki_size float-left ml-sm-5 pr-3" style="border-width: 0px 0px 3px 0px;">
+			<span class="sc_tuki">12</span><span class="sc_tuki2">月</span>
 		</div>
 
 		<div class="top_border_y float-left m-0 p-0" style="border-width: 0px 0px 0px 3px;"></div>
 
-		<div class="top_border_x lh15 float-left pl-3 pt-3" style="border-width: 0px 0px 3px 0px; width: 700px;">
+		<div class="top_border_x sc_detail_size lh15" style="border-width: 0px 0px 3px 0px;">
 			<span style="color: #ed1c24;">■</span>寄せ植え体験（要予約）<br />
 			12/1～12/15<br />
 		</div>

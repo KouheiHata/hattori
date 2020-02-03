def989da70658ffa42bda84b98385ef7c613c1cf
hirotaka <h_yamaguchi@standtogether-fukushi.com>
2020-02-03 13:30:09 +0900

    花苗ページ、スマホ版調整

--- page-hananae.php
+++ page-hananae.php
@@ -8,7 +8,7 @@ Template Name: hananae
 <?php get_header(); ?>
 
 <img src="<?php echo esc_url( get_theme_file_uri('images/main-img4.jpg') ); ?>" alt="季節ごとの取り扱い花苗を紹介します！">
-
+<!-- 花の画像は同じ高さ（haight）推奨2:1.5 -->
 
 <div class="container">
     <div class="row">
@@ -16,8 +16,8 @@ Template Name: hananae
 
         <div class="col-lg-12 col-md-12 col-sm-12">
          <div class="container">
-             <div class="row">
-                 <div class="col-lg-4 col-md-4 col-sm-12 px-5">
+             <div class="row justify-content-between">
+                 <div class="col-lg-4 col-md-4 col-sm-6">
                      <h2 class="flower_h">４月</h2>
                      <img class="f-img" src="<?php echo esc_url( get_theme_file_uri('images/petunia.jpg') ); ?>" alt="">
                      <table class="table f-table">
@@ -34,7 +34,7 @@ Template Name: hananae
                          </tbody>
                      </table>
                  </div>
-                 <div class="col-lg-4 col-md-4 col-sm-12 px-5">
+                 <div class="col-lg-4 col-md-4 col-sm-6">
                      <h2 class="flower_h">10月</h2>
                      <img src="<?php echo esc_url( get_theme_file_uri('images/begonia.jpg')); ?>" alt="" class="f-img">
                      <table class="table f-table">
@@ -53,7 +53,7 @@ Template Name: hananae
                          </tbody>
                      </table>
                  </div>
-                 <div class="col-lg-4 col-md-4 col-sm-12 px-5">
+                 <div class="col-lg-4 col-md-4 col-sm-6">
                      <h2 class="flower_h">11月</h2>
                      <img src="<?php echo esc_url( get_theme_file_uri('images/pangy.jpg') ); ?>" alt="" class="f-img">
                      <table class="table f-table">
@@ -78,11 +78,11 @@ Template Name: hananae
 
 <h4 class="section_h" id="cut-sap">切花苗</h4>
 <div class="col-lg-12 col-md-12 col-sm-12">
+  <h2 class="flower_h">７月</h2>
    
     <div class="container">
-        <div class="row">
-           <h2 class="flower_h w-90">７月</h2>
-            <div class="col-lg-4 col-md-4 col-sm-12 px-5">
+        <div class="row justify-content-between">
+            <div class="col-lg-4 col-md-4 col-sm-6">
     <img src="<?php echo esc_url( get_theme_file_uri('images/zinnia.jpg') ); ?>" alt="百日草" class="f-img">
     <table class="table f-table">
         <thead class="f-name">
@@ -98,7 +98,7 @@ Template Name: hananae
         </thead>
     </table>
 </div>
-<div class="col-lg-4 col-md-4 col-sm-12 px-5">
+<div class="col-lg-4 col-md-4 col-sm-6">
     <img src="<?php echo esc_url( get_theme_file_uri('images/amaranth.jpg') ); ?>" alt="千日草" class="f-img">
     <table class="table f-table">
         <thead class="f-name">
@@ -116,7 +116,7 @@ Template Name: hananae
         </thead>
     </table>
 </div>
-<div class="col-lg-4 col-md-4 col-sm-12 px-5">
+<div class="col-lg-4 col-md-4 col-sm-6">
     <img src="<?php echo esc_url( get_theme_file_uri('images/aster01_02.jpg') ); ?>" alt="アスター" class="f-img">
     <table class="table f-table">
         <thead class="f-name">
@@ -141,10 +141,11 @@ Template Name: hananae
 
 <h4 class="section_h" id="daisy-sap">菊苗</h4>
         <div class="col-lg-12 col-md-12 col-sm-12">
+           <h2 class="flower_h">１０月</h2>
             <div class="container">
                 <div class="row">
-                   <h2 class="flower_h w-90">１０月</h2>
-                    <div class="col-lg-4 col-md-4 col-sm-12 px-5">
+                   
+                    <div class="col-lg-4 col-md-4 col-sm-6">
                         <img src="<?php echo esc_url( get_theme_file_uri('images/potmom.jpg') ); ?>" alt="ポットマム" class="f-img">
                         <table class="table f-table">
                             <thead class="f-name">
@@ -162,7 +163,7 @@ Template Name: hananae
                             </thead>
                         </table>
                     </div>
-                    <div class="col-lg-4 col-md-4 col-sm-12 px-5 pb-5">
+                    <div class="col-lg-4 col-md-4 col-sm-6">
                         <img src="<?php echo esc_url( get_theme_file_uri('images/cushionmom.jpg') ); ?>" alt="クッションマム" class="f-img">
                         <table class="table f-table">
                             <thead class="f-name">

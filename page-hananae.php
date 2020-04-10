b70091b87bd4c1168a0950d04d07e746b75d490f
Kouhei Hata <k_hata@standtogether-fukushi.com>
2020-04-10 16:27:27 +0900

    花苗と野菜
    投稿の「今月の取り扱い野菜」を削除。

--- page-hananae.php
+++ page-hananae.php
@@ -8,7 +8,7 @@ Template Name: hananae
 <?php get_header(); ?>
 
 <img src="<?php echo esc_url( get_theme_file_uri('images/main-img4.jpg') ); ?>" alt="季節ごとの取り扱い花苗を紹介します！">
-<!-- 花の画像は同じ高さ（haight）推奨2:1.5 -->
+<!-- 花の画像は同じ高さ（hight）推奨2:1.5 -->
 
 <div class="container">
     <div class="row">
@@ -113,96 +113,174 @@ Template Name: hananae
 <div class="col-lg-12 col-md-12 col-sm-12">
     <div class="container">
         <div class="row justify-content-between">
-            <div class="col-lg-4 col-md-4 col-sm-6 pt-5">
-    <img src="<?php echo esc_url( get_theme_file_uri('images/zinnia.jpg') ); ?>" alt="百日草" class="f-img">
-    <table class="table f-table">
-        <thead class="f-name">
-            <tr>
-                <th>百日草</th>
-                <tbody>
-                    <tr>
-                        <td>色：赤　白　黄　桃</td>
-                    </tr>
-                </tbody>
-            </tr>
-        </thead>
-    </table>
-</div>
-<div class="col-lg-4 col-md-4 col-sm-6 pt-5">
-    <img src="<?php echo esc_url( get_theme_file_uri('images/amaranth.jpg') ); ?>" alt="千日草" class="f-img">
-    <table class="table f-table">
-        <thead class="f-name">
-            <tr>
-                <th>千日草</th>
-                <tbody>
-                    <tr>
-                        <td>色：赤　白　黄　紫</td>
-                    </tr>
-                </tbody>
-            </tr>
-        </thead>
-    </table>
-</div>
-<div class="col-lg-4 col-md-4 col-sm-6 pt-5">
-    <img src="<?php echo esc_url( get_theme_file_uri('images/aster01_02.jpg') ); ?>" alt="アスター" class="f-img">
-    <table class="table f-table">
-        <thead class="f-name">
-            <tr>
-                <th>アスター</th>
-                <tbody>
-                    <tr>
-                        <td>色：－－</td>
-                    </tr>
-                </tbody>
-            </tr>
-        </thead>
-    </table>
-</div>
-        </div>
-    </div>
-</div>
+             <div class="col-lg-4 col-md-4 col-sm-6 pt-5">
+                 <img src="<?php echo esc_url( get_theme_file_uri('images/hananae_asuta.jpg') ); ?>" alt="" class="f-img">
+                 <table class="table f-table">
+                     <thead class="f-name">
+                         <tr>
+                             <th>アスター</th>
+                         </tr>
+                     </thead>
+                     <tbody>
+                         <tr>
+                             <td>４月下旬～５月中旬</td>
+                         </tr>
+                     </tbody>
+                 </table>
+             </div>
+             <div class="col-lg-4 col-md-4 col-sm-6 pt-5">
+                 <img src="<?php echo esc_url( get_theme_file_uri('images/hananae_hyakunichisou.jpg') ); ?>" alt="" class="f-img">
+                 <table class="table f-table">
+                     <thead class="f-name">
+                         <tr>
+                             <th>百日草</th>
+                         </tr>
+                     </thead>
+                     <tbody>
+                         <tr>
+                             <td>４月下旬～５月中旬</td>
+                         </tr>
+                     </tbody>
+                 </table>
+             </div>
+             <div class="col-lg-4 col-md-4 col-sm-6 pt-5">
+                 <img src="<?php echo esc_url( get_theme_file_uri('images/hananae_sennichisou.jpg') ); ?>" alt="" class="f-img">
+                 <table class="table f-table">
+                     <thead class="f-name">
+                         <tr>
+                             <th>千日紅</th>
+                         </tr>
+                     </thead>
+                     <tbody>
+                         <tr>
+                             <td>４月下旬～５月中旬</td>
+                         </tr>
+                     </tbody>
+                 </table>
+             </div>
+             <div class="col-lg-4 col-md-4 col-sm-6 pt-5">
+                 <img src="<?php echo esc_url( get_theme_file_uri('images/hananae_starchis.jpg') ); ?>" alt="" class="f-img">
+                 <table class="table f-table">
+                     <thead class="f-name">
+                         <tr>
+                             <th>スターチス</th>
+                         </tr>
+                     </thead>
+                     <tbody>
+                         <tr>
+                             <td>４月下旬～５月中旬</td>
+                         </tr>
+                     </tbody>
+                 </table>
+             </div>
+             <div class="col-lg-4 col-md-4 col-sm-6 pt-5">
+                 <img src="<?php echo esc_url( get_theme_file_uri('images/hananae_ageratam.jpg') ); ?>" alt="" class="f-img">
+                 <table class="table f-table">
+                     <thead class="f-name">
+                         <tr>
+                             <th style="font-size: 15px;">アゲラタム（トップブルー）</th>
+                         </tr>
+                     </thead>
+                     <tbody>
+                         <tr>
+                             <td>４月下旬～５月中旬</td>
+                         </tr>
+                     </tbody>
+                 </table>
+             </div>
+             <div class="col-lg-4 col-md-4 col-sm-6">
+             </div>
+        </div> <!-- row -->
+    </div> <!-- container -->
+</div> <!-- col-12 -->
 
 
-<h4 class="section_h" id="daisy-sap">菊苗</h4>
-        <div class="col-lg-12 col-md-12 col-sm-12">
-            <div class="container">
-                <div class="row">
-                   
-                    <div class="col-lg-4 col-md-4 col-sm-6 pt-5">
-                        <img src="<?php echo esc_url( get_theme_file_uri('images/potmom.jpg') ); ?>" alt="ポットマム" class="f-img">
-                        <table class="table f-table">
-                            <thead class="f-name">
-                                <tr>
-                                    <th>ポットマム</th>
-                                    <tbody>
-                                        <tr>
-                                            <td>色：－－</td>
-                                        </tr>
-                                    </tbody>
-                                </tr>
-                            </thead>
-                        </table>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-6 pt-5">
-                        <img src="<?php echo esc_url( get_theme_file_uri('images/cushionmom.jpg') ); ?>" alt="クッションマム" class="f-img">
-                        <table class="table f-table">
-                            <thead class="f-name">
-                                <tr>
-                                    <th>クッションマム</th>
-                                    <tbody>
-                                        <tr>
-                                            <td>色：－－</td>
-                                        </tr>
-                                    </tbody>
-                                </tr>
-                            </thead>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
+<h4 class="section_h" id="daisy-sap">花・菊</h4>
+    <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="container">
+            <div class="row">
+	             <div class="col-lg-4 col-md-4 col-sm-6 pt-5">
+	                 <img src="<?php echo esc_url( get_theme_file_uri('images/hananae_potmum.jpg') ); ?>" alt="" class="f-img">
+	                 <table class="table f-table">
+	                     <thead class="f-name">
+	                         <tr>
+	                             <th>ポットマム</th>
+	                         </tr>
+	                     </thead>
+	                     <tbody>
+	                         <tr>
+	                             <td>10月上旬～11月上旬</td>
+	                         </tr>
+	                     </tbody>
+	                 </table>
+	             </div>
+	             <div class="col-lg-4 col-md-4 col-sm-6 pt-5">
+	                 <img src="<?php echo esc_url( get_theme_file_uri('images/hananae_gardenmum.jpg') ); ?>" alt="" class="f-img">
+	                 <table class="table f-table">
+	                     <thead class="f-name">
+	                         <tr>
+	                             <th>ガーデンマム</th>
+	                         </tr>
+	                     </thead>
+	                     <tbody>
+	                         <tr>
+	                             <td>10月上旬～11月上旬</td>
+	                         </tr>
+	                     </tbody>
+	                 </table>
+	             </div>
+	             <div class="col-lg-4 col-md-4 col-sm-6 pt-5">
+	                 <img src="<?php echo esc_url( get_theme_file_uri('images/hananae_kankiku.jpg') ); ?>" alt="" class="f-img">
+	                 <table class="table f-table">
+	                     <thead class="f-name">
+	                         <tr>
+	                             <th>寒菊苗</th>
+	                         </tr>
+	                     </thead>
+	                     <tbody>
+	                         <tr>
+	                             <td>10月上旬～11月上旬</td>
+	                         </tr>
+	                     </tbody>
+	                 </table>
+	             </div>
+	             <div class="col-lg-4 col-md-4 col-sm-6 pt-5">
+	                 <img src="<?php echo esc_url( get_theme_file_uri('images/hananae_akikiku.jpg') ); ?>" alt="" class="f-img">
+	                 <table class="table f-table">
+	                     <thead class="f-name">
+	                         <tr>
+	                             <th>秋菊苗</th>
+	                         </tr>
+	                     </thead>
+	                     <tbody>
+	                         <tr>
+	                             <td>10月上旬～11月上旬</td>
+	                         </tr>
+	                     </tbody>
+	                 </table>
+	             </div>
+	             <div class="col-lg-4 col-md-4 col-sm-6 pt-5">
+	                 <img src="<?php echo esc_url( get_theme_file_uri('images/hananae_shokuyougiku.jpg') ); ?>" alt="" class="f-img">
+	                 <table class="table f-table">
+	                     <thead class="f-name">
+	                         <tr>
+	                             <th>食用菊苗</th>
+	                         </tr>
+	                     </thead>
+	                     <tbody>
+	                         <tr>
+	                             <td>10月上旬～11月上旬</td>
+	                         </tr>
+	                     </tbody>
+	                 </table>
+	             </div>
+	             <div class="col-lg-4 col-md-4 col-sm-6">
+	             </div>
+            </div> <!-- row -->
+        </div> <!-- container -->
+    </div> <!-- col-12 -->
         
-        <section class="section">
+        <section class="section" style="margin-top: 60px;margin-bottom: 60px;">
     <p class="main_p">ご注文は下記注文票をダウンロードしてご希望の品目を記入し、<br>ＦＡＸか当ハウスまで直接お持ちください。</p>
     <a class="d_g_pdf" href="#">2019年 苗注文票のダウンロードはこちら<i class="far fa-file-pdf"></i></a>
 </section>
--- page-yasai.php
+++ page-yasai.php
@@ -34,39 +34,119 @@ Template Name: yasai
                         <p class="card-p"><i class="fas fa-phone"></i>0778-23-3511</p>
                         <iframe class="gmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3232.2550622198405!2d136.15819831461175!3d35.89176832600518!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5ff8ac5d7588c8a7%3A0xfe1659a2275e2828!2z55m-5aeT44Gu6aSo5Y2X5bqX!5e0!3m2!1sja!2sjp!4v1576455835310!5m2!1sja!2sjp" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
                     </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+                        <h5 class="card-title">百姓の館&nbsp;村国店</h5>
+                        <p class="card-p">〒915-0084</p>
+                        <p class="card-p">福井県越前市村国2-12-41</p>
+                        <p class="card-p"><i class="fas fa-phone"></i>0778-23-4170</p>
+                        <iframe class="gmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d807.9206243475725!2d136.17839448806012!3d35.90579108502422!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5ff8ac2453caba0f%3A0x4f6041522e98a628!2z55m-5aeT44Gu6aSo5p2R5Zu95bqX!5e0!3m2!1sja!2sjp!4v1586494681267!5m2!1sja!2sjp" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+                        <h5 class="card-title">百姓の館&nbsp;村国&nbsp;南&nbsp;シピィ店</h5>
+                        <p class="card-p">〒915-0883&nbsp;福井県越前市新町7-8 SIPY(シピィ) 1F</p>
+                        <p class="card-p"><i class="fas fa-phone"></i>0778-21-1318</p>
+                        <iframe class="gmap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3231.7580362213803!2d136.14963064988464!3d35.90394132523729!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5ff8ac4b1bbcdb23%3A0x546a5e78b577aa0b!2z55m-5aeT44Gu6aSoIOOCt-ODlOOCo-W6lw!5e0!3m2!1sja!2sjp!4v1586494981140!5m2!1sja!2sjp" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                    </div>
+                    <div class="col-lg-4 col-md-6 col-sm-6">
+                    </div>
 
-    <h4 class="section_h" id="this_m">今月の取り扱い野菜</h4>
+	<div class="col-lg-12 col-md-12 col-sm-12">
+	    <h4 class="section_h">取り扱い野菜</h4>
+	</div>
 
-        <!-- ループ開始 -->
-        <?php
-	$args = Array(
-		'post_type' => 'yasai',
-		'post_status' => 'publish',
-		'post_per_page' => 8
-	);
+	<div class="col-lg-12 col-md-12 col-sm-12 pt-5">
+		<h2 class="yasai_h_tuki">６月～</h2>
+	</div>
 
-	$wp_query = new WP_Query($args);
 
-	if ( $wp_query->have_posts() ) {
-		while ( $wp_query->have_posts() ) {
-			$wp_query->the_post(); ?>
-
-       <!-- 投稿する画像は300×200推奨 -->
-        <!-- ループする部分 -->
-            <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
-            <div class="yasai-img"><?php the_content(); ?></div>
-            <h5 class="card-title mt-3"><?php the_title(); ?></h5>
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_6_kyabetsu.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">キャベツ</h5>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_6_burokkori.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">ブロッコリー</h5>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_6_karifurawa.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">カリフラワー</h5>
         </div>
 
-        <?php
-	}
-}
+	<div class="col-lg-12 col-md-12 col-sm-12 pt-5">
+		<h2 class="yasai_h_tuki">７月～</h2>
+	</div>
 
-// 投稿データのリセット
-wp_reset_postdata();
- ?>
-        <!-- ループ終了 -->
-    </div>
-</div>
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_7_nasu.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">ナス</h5>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_7_pi-man.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">ピーマン</h5>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_7_tomato.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">トマト</h5>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_7_kyuri.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">きゅうり</h5>
+        </div>
+
+	<div class="col-lg-12 col-md-12 col-sm-12 pt-5">
+		<h2 class="yasai_h_tuki">８月～</h2>
+	</div>
+
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_8_okura.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">オクラ</h5>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_8_toumorokoshi.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">とうもろこし</h5>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6">
+        </div>
+
+	<div class="col-lg-12 col-md-12 col-sm-12 pt-5">
+		<h2 class="yasai_h_tuki">10月下旬～11月上旬</h2>
+	</div>
+
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_10_shokuyougiku.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">食用菊</h5>
+        </div>
+
+	<div class="col-lg-12 col-md-12 col-sm-12 pt-5">
+		<h2 class="yasai_h_tuki">12月～</h2>
+	</div>
+
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_12_o-tamupoemu.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">オータムポエム</h5>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_12_sironegi.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">白ネギ</h5>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_12_karifurawa.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">カリフラワー</h5>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_12_hakusai.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">白菜</h5>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_12_kyabetsu.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">キャベツ</h5>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 pt-5 pb-5">
+            <div class="yasai-img"><img src="<?php echo esc_url( get_theme_file_uri('images/yasai_12_burokkori.png') ); ?>" alt=""></div>
+            <h5 class="card-title mt-3">ブロッコリー</h5>
+        </div>
+
+    </div> <!-- row -->
+</div> <!-- container -->
 
 <?php get_footer(); ?>
